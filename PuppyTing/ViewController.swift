7bcdf6b79194aef797b921301f5d43b08d786d21
kseunghui@gmail.com <kseunghui@gmail.com>
2024-08-26 16:14:58 +0900

    Initial Commit

---
+++ PuppyTing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PuppyTing
+//
+//  Created by 김승희 on 8/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
