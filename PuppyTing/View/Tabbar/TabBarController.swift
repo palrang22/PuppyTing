c50de1f6a8652043dcc72075d2698d2d9ee95b0f
sh990920 <sh990920@gmail.com>
2024-08-27 10:20:35 +0900

    [fix] 줄 맞춤 수정

--- PuppyTing/View/Tabbar/TabBarController.swift
+++ PuppyTing/View/Tabbar/TabBarController.swift
@@ -8,35 +8,35 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.tabBar.tintColor = .puppyPurple
     
-    let tingVC = UINavigationController(rootViewController: TingViewController())
-    tingVC.tabBarItem = UITabBarItem(
-      title: "퍼피팅",
-      image: UIImage(systemName: "dog"),
-      tag: 0
-    )
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = .puppyPurple
+        
+        let tingVC = UINavigationController(rootViewController: TingViewController())
+        tingVC.tabBarItem = UITabBarItem(
+            title: "퍼피팅",
+            image: UIImage(systemName: "dog"),
+            tag: 0
+        )
+        
+        let chatVC = UINavigationController(rootViewController: ChatViewController())
+        chatVC.tabBarItem = UITabBarItem(
+            title: "채팅",
+            image: UIImage(systemName: "bubble.left.and.bubble.right"),
+            tag: 1
+        )
+        
+        let myPageVC = UINavigationController(rootViewController: MypageViewController())
+        myPageVC.tabBarItem = UITabBarItem(
+            title: "마이페이지",
+            image: UIImage(systemName: "person"),
+            tag: 2
+        )
+        
+        self.setViewControllers([tingVC, chatVC, myPageVC], animated: true)
+        // Do any additional setup after loading the view.
+        
+    }
     
-    let chatVC = UINavigationController(rootViewController: ChatViewController())
-    chatVC.tabBarItem = UITabBarItem(
-      title: "채팅",
-      image: UIImage(systemName: "bubble.left.and.bubble.right"),
-      tag: 1
-    )
-    
-    let myPageVC = UINavigationController(rootViewController: MypageViewController())
-    myPageVC.tabBarItem = UITabBarItem(
-      title: "마이페이지",
-      image: UIImage(systemName: "person"),
-      tag: 2
-    )
-    
-    self.setViewControllers([tingVC, chatVC, myPageVC], animated: true)
-    // Do any additional setup after loading the view.
-    
-  }
-  
 }
