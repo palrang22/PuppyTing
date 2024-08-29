5c98e7cc214dada46dfe6227b16138de014d2d58
sh990920 <sh990920@gmail.com>
2024-08-29 18:54:05 +0900

    [feat] ViewModel에 데이터 추가 로직 생성

--- PuppyTing/ViewModel/Login/LoginViewModel.swift
+++ PuppyTing/ViewModel/Login/LoginViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+import RxSwift
+import FirebaseAuth
+import GoogleSignIn
+
+class LoginViewModel {
+    private let disposeBag = DisposeBag()
+    
+    let userSubject = PublishSubject<User>()
+    let errorSubject = PublishSubject<Error>()
+    
+    func googleSignIn(viewController: UIViewController) {
+        FirebaseAuthManager.shared.googleSignIn(viewController: viewController).observe(on: MainScheduler.instance).subscribe(onSuccess: { [weak self] user in
+            self?.userSubject.onNext(user)
+        }, onFailure: { [weak self] error in
+            self?.errorSubject.onNext(error)
+        }).disposed(by: disposeBag)
+    }
+}
