b46ab08a12b255f9fda6e588c1c9707429b2e5cd
sh990920 <sh990920@gmail.com>
2024-08-29 18:51:20 +0900

    [feat] FirebaseAuthManager 생성

--- PuppyTing/Common/FirebaseAuthManager.swift
+++ PuppyTing/Common/FirebaseAuthManager.swift
@@ -6,3 +6,116 @@
 //
 
 import Foundation
+import UIKit
+
+import FirebaseAuth
+import FirebaseCore
+import RxSwift
+import GoogleSignIn
+
+enum AuthError: Error {
+    case CreateFailError
+    case SendEmailFailError
+    case SignInFailError
+    case EmailVerificationFailError
+    case ClientIdInvaildError
+    case GoogleSignInFailError
+    case TokenInvaildError
+}
+
+class FirebaseAuthManager {
+    
+    static let shared = FirebaseAuthManager()
+    
+    private init() {
+        
+    }
+    
+    func emailSignUp(email: String, pw: String) -> Single<User> {
+        return Single<User>.create { [weak self] single in
+            Auth.auth().createUser(withEmail: email, password: pw) { result, error in
+                if let error = error {
+                    single(.failure(error))
+                    return
+                }
+                
+                guard let result = result else {
+                    single(.failure(AuthError.CreateFailError))
+                    return
+                }
+                
+                if !result.user.isEmailVerified {
+                    Auth.auth().currentUser?.sendEmailVerification { error in
+                        if let error = error {
+                            single(.failure(AuthError.SendEmailFailError))
+                            return
+                        } else {
+                            print("이메일 전송 성공")
+                            single(.success(result.user))
+                        }
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func emailSignIn(email: String, pw: String) -> Single<User> {
+        return Single<User>.create { [weak self] single in
+            Auth.auth().signIn(withEmail: email, password: pw) { [weak self] result, error in
+                if let error = error {
+                    print("에러 발생: \(error)")
+                    single(.failure(AuthError.SignInFailError))
+                }
+                if let result = result {
+                    if result.user.isEmailVerified {
+                        single(.success(result.user))
+                    } else {
+                        single(.failure(AuthError.EmailVerificationFailError))
+                    }
+                } else {
+                    single(.failure(AuthError.SignInFailError))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func googleSignIn(viewController: UIViewController) -> Single<User> {
+        return Single<User>.create { [weak self] single in
+            guard let clientId = FirebaseApp.app()?.options.clientID else { 
+                single(.failure(AuthError.ClientIdInvaildError))
+                return Disposables.create()
+            }
+            let config = GIDConfiguration(clientID: clientId)
+            GIDSignIn.sharedInstance.configuration = config
+            
+            GIDSignIn.sharedInstance.signIn(withPresenting: viewController) { [unowned self] result, error in
+                guard error == nil else {
+                    print("test1: \(String(describing: error))")
+                    single(.failure(AuthError.GoogleSignInFailError))
+                    return
+                }
+                
+                guard let user = result?.user, let idToken = user.idToken?.tokenString else {
+                    single(.failure(AuthError.TokenInvaildError))
+                    return
+                }
+                
+                let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+                
+                Auth.auth().signIn(with: credential) { result, error in
+                    if let error = error {
+                        print("test2")
+                        single(.failure(AuthError.GoogleSignInFailError))
+                    }
+                    if let result = result {
+                        single(.success(result.user))
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+}
