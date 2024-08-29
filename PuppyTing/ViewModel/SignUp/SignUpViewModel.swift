4fe110dfacc2a49413e03a0e0257f0424a05f63c
sh990920 <sh990920@gmail.com>
2024-08-29 18:54:46 +0900

    [feat] SignUpViewModel 에 데이터 추가 로직 추가

--- PuppyTing/ViewModel/SignUp/SignUpViewModel.swift
+++ PuppyTing/ViewModel/SignUp/SignUpViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+import RxSwift
+import FirebaseAuth
+
+class SignUpViewModel {
+    
+    private let disposeBag = DisposeBag()
+    
+    let userSubject = PublishSubject<User>()
+    let userErrorSubject = PublishSubject<Error>()
+    let memeberSubject = PublishSubject<Member>()
+    let memberErrorSubject = PublishSubject<Error>()
+    
+    func authentication(email: String, pw: String) {
+        FirebaseAuthManager.shared.emailSignUp(email: email, pw: pw).observe(on: MainScheduler.instance).subscribe(onSuccess: { [weak self] user in
+            self?.userSubject.onNext(user)
+        }, onFailure: { error in
+            self.userErrorSubject.onNext(error)
+        }).disposed(by: disposeBag)
+    }
+    
+    func signUp(uuid: String, email: String, pw: String, nickname: String) {
+        FireStoreDatabaseManager.shared.emailSignUp(uuid: uuid, email: email, pw: pw, nickname: nickname).observe(on: MainScheduler.instance).subscribe(onSuccess: { [weak self] memeber in
+            self?.memeberSubject.onNext(memeber)
+        }, onFailure: { error in
+            self.memberErrorSubject.onNext(error)
+        }).disposed(by: disposeBag)
+    }
+    
+    
+}
