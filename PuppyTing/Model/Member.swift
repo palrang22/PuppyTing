7b5992e090592593b80f72e5498157a8339e8494
sh990920 <sh990920@gmail.com>
2024-08-29 18:51:39 +0900

    [feat] Member 구조체 생성

--- PuppyTing/Model/Member.swift
+++ PuppyTing/Model/Member.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct Member: Codable {
+    let uuid: String
+    let email: String
+    let password: String
+    let nickname: String
+    let profileImage: String
+    let footPrint: Int
+    let isSocial: Bool
+    
+    var dictionary: [String: Any] {
+        return [
+            "uuid": uuid,
+            "email": email,
+            "password": password,
+            "nickname": nickname,
+            "profileImage": profileImage,
+            "footPrint": footPrint,
+            "isSocial": isSocial
+        ]
+    }
+}
