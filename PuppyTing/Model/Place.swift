3a2ba9ed7d1ef9598a1cf6dac1fbccccdf12fa7b
kseunghui@gmail.com <kseunghui@gmail.com>
2024-09-01 21:28:53 +0900

    [feat] 장소 모델 생성

--- PuppyTing/Model/Place.swift
+++ PuppyTing/Model/Place.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct KakaoLocalSearchResponse: Codable {
+    let documents: [Place]
+}
+
+struct Place: Codable {
+    let placeName: String
+    let roadAddressName: String
+    let x: String
+    let y: String
+    let distance: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case placeName = "place_name"
+        case roadAddressName = "road_address_name"
+        case x, y, distance
+    }
+}
