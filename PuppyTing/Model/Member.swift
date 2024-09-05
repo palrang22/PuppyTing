b559cd5107fa8e284c0c038fa174244cb995decb
sh990920 <sh990920@gmail.com>
2024-09-05 15:12:33 +0900

    [feat] 내 정보 불러오기

--- PuppyTing/Model/Member.swift
+++ PuppyTing/Model/Member.swift
@@ -16,6 +16,34 @@ struct Member: Codable {
     let footPrint: Int
     let isSocial: Bool
     
+    init(uuid: String, email: String, password: String, nickname: String, profileImage: String, footPrint: Int, isSocial: Bool) {
+        self.uuid = uuid
+        self.email = email
+        self.password = password
+        self.nickname = nickname
+        self.profileImage = profileImage
+        self.footPrint = footPrint
+        self.isSocial = isSocial
+    }
+    
+    init?(dictionary: [String: Any]) {
+        guard let uuid = dictionary["uuid"] as? String,
+              let email = dictionary["email"] as? String,
+              let password = dictionary["password"] as? String,
+              let nickname = dictionary["nickname"] as? String,
+              let profileImage = dictionary["profileImage"] as? String,
+              let footPrint = dictionary["footPrint"] as? Int,
+              let isSocial = dictionary["isSocial"] as? Bool else { return nil }
+        
+        self.uuid = uuid
+        self.email = email
+        self.password = password
+        self.nickname = nickname
+        self.profileImage = profileImage
+        self.footPrint = footPrint
+        self.isSocial = isSocial
+    }
+    
     var dictionary: [String: Any] {
         return [
             "uuid": uuid,
