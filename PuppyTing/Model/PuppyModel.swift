9e0347dabece0fba74e274255f3de2f8da45d815
palrang22 <kseunghui@gmail.com>
2024-09-09 03:43:47 +0900

    [chore] 모델 변경

---
+++ PuppyTing/Model/PuppyModel.swift
@@ -0,0 +1,17 @@
+//
+//  PuppyModel.swift
+//  PuppyTing
+//
+//  Created by t2023-m0072 on 9/9/24.
+//
+
+import Foundation
+
+struct PuppyModel {
+    let puppyId: String
+    let age: Int
+    let name: String
+    let petImage: String
+    let tag: [String]
+    let userId: String
+}
