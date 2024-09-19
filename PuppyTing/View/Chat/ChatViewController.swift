57683cbfee10114bf2a493a30fffa9d7711395f7
gahyejeon <gahyejeon@gmail.com>
2024-09-20 00:37:40 +0900

    [feat] 채팅방에서 상대프로필이미지 클릭시 프로필모달연결

--- PuppyTing/View/Chat/ChatViewController.swift
+++ PuppyTing/View/Chat/ChatViewController.swift
@@ -161,6 +161,10 @@ class ChatViewController: UIViewController {
                         .observe(on: MainScheduler.instance)
                         .subscribe(onNext: { member in
                             cell.config(image: member.profileImage, message: message.text, time: dateString, nickname: member.nickname)
+                            // 프로필 이미지 탭 시 액션 설정 - jgh
+                            cell.profileImageTapped = { [weak self] in
+                                self?.presentProfileViewController(senderId: message.senderId)
+                            }
                         }).disposed(by: self.disposeBag)
                     return cell
                 }
@@ -181,10 +185,11 @@ class ChatViewController: UIViewController {
             .disposed(by: disposeBag)
     }
     
-    // 하프모달로 띄우기
-    private func presentProfileViewController() {
+    // 하프모달로 띄우기 - jgh
+    private func presentProfileViewController(senderId: String) {
         let profileVC = ProfileViewController()
         profileVC.modalPresentationStyle = .pageSheet
+        profileVC.userid = senderId
         if let sheet = profileVC.sheetPresentationController {
             sheet.detents = [.medium()]
             sheet.prefersGrabberVisible = true
